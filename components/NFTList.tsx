60b1b178a40b2d2947cd2d8362c0d19bae68b765
Miguel Graca <miguelportelagraca@gmail.com>
2023-03-06 21:16:09 +0000

    NFT List component creation and placeholder

---
+++ components/NFTList.tsx
@@ -0,0 +1,81 @@
+const NFTList = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="grid grid-flow-row grid-cols-3 gap-10 font-mono text-white text-sm text-center font-bold leading-6 bg-stripes-pink rounded-lg">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="/placeholder.webp" alt="Shoes" />
+          </figure>
+          <div className="card-body bg-black">
+            <h2 className="card-title">NFT</h2>
+            <p>NFT 1</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="/placeholder.webp" alt="Shoes" />
+          </figure>
+          <div className="card-body bg-black">
+            <h2 className="card-title">NFT</h2>
+            <p>NFT 2</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="/placeholder.webp" alt="Shoes" />
+          </figure>
+          <div className="card-body bg-black">
+            <h2 className="card-title">NFT</h2>
+            <p>NFT 3</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="/placeholder.webp" alt="Shoes" />
+          </figure>
+          <div className="card-body bg-black">
+            <h2 className="card-title">NFT</h2>
+            <p>NFT 4</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="/placeholder.webp" alt="Shoes" />
+          </figure>
+          <div className="card-body bg-black">
+            <h2 className="card-title">NFT</h2>
+            <p>NFT 5</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="/placeholder.webp" alt="Shoes" />
+          </figure>
+          <div className="card-body bg-black">
+            <h2 className="card-title">NFT</h2>
+            <p>NFT 6</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default NFTList;
